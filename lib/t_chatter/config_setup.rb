ee99c6579eada13be9528c5f10200524a72ee12c
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-10-17 20:45:10 +0100

    working around file errors

--- lib/t_chatter/config_setup.rb
+++ lib/t_chatter/config_setup.rb
@@ -11,11 +11,11 @@ module TChatter
           config_file = "#{file_source}/#{CONFIG_FILE}"
           break if File.exists? config_file
       }
-      @@config_data = load_config(config_file)
+      @@config_data = load_config(config_file) || Hash.new(nil)
     end
 
     def self.load_config(file_path)
-      ::YAML.load_file(file_path) || Hash.new(nil)
+      ::YAML.load_file(file_path)
     end
 
     def configuration
