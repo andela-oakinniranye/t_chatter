0726ac6460b960aee80c6d9da4049a80fb3872b7
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-10-17 21:39:54 +0100

    finished fixing errors relating to URL

--- lib/t_chatter.rb
+++ lib/t_chatter.rb
@@ -6,6 +6,7 @@ require 'yaml'
 require 'singleton'
 
 module TChatter
+  DEFAULT_URL = "https://tchatter.herokuapp.com"
   # Your code goes here...
 end
 
@@ -18,3 +19,5 @@ end
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 Dir[APP_ROOT.join('lib', 't_chatter', '*.rb')].each { |f| require f }
+CS = TChatter::ConfigSetup
+TC = TChatter::Chat
--- lib/t_chatter/chat.rb
+++ lib/t_chatter/chat.rb
@@ -1,6 +1,7 @@
 module TChatter
   class Chat
     attr_reader :connection, :messages, :response, :last_message_id, :user, :config
+    # attr_accessor :url
 
     def initialize(username, config_class)
       @user = username
@@ -12,7 +13,21 @@ module TChatter
     end
 
     def set_url
-      @url = @config[:host] || 'http://localhost'
+      begin
+        @url = URI.parse(@config[:host])
+      rescue URI::InvalidURIError
+        puts <<-EOS
+        You dont seem to have a valid URL in your .chatter.yml file\n
+        You may run ``setup_t_chatter`` to set a default url which will be
+        used everytime you try to connect. However you are now being connected
+        to the global chatter stream
+        EOS
+        @url = URI.parse(TChatter::DEFAULT_URL)
+      end
+    end
+
+    def unknown_url
+      puts "The URL doesn't appear to be valid a URL"
     end
 
 
